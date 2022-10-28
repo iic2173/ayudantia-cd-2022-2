712b7d4fd1faf288ff06710151505f185920775e
Franco Giannoni <fagiannoni@uc.cl>
2022-10-28 11:19:01 -0300

    hola hubo cambiossss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sigue funcionadooooo!
+          Sigue funcionadoooooooo!
         </p>
         <a
           className="App-link"
